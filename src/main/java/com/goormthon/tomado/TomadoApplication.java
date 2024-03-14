1b50077664b2cc7f336192b7316db3753ad97860
soyeonvv <sovvworld@gmail.com>
2024-03-15 01:50:36 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/goormthon/tomado/TomadoApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.tomado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goormthon/tomado/TomadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.tomado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
