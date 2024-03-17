4ebdd34d5c0f96b576bb40e5da075a998d2ea892
shin0112 <wnslcosltimo12@gmail.com>
2024-03-18 00:21:03 +0900

    add: exception handler filter

---
+++ src/main/java/com/goormthon/tomado/common/response/ExceptionHandlerFilter.java
@@ -0,0 +1,50 @@
+package com.goormthon.tomado.common.response;
+
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goormthon.tomado.common.ApiResponse;
+import com.goormthon.tomado.common.exception.BaseException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (BaseException exception) {
+            setErrorResponse(response, exception.getError());
+        }
+    }
+
+    private void setErrorResponse(
+            HttpServletResponse response,
+            ErrorMessage errorMessage
+    ){
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(errorMessage.getHttpStatusValue());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        ApiResponse baseResponse = ApiResponse.error(errorMessage);
+
+        try {
+            response.getWriter()
+                    .write(objectMapper.configure(JsonWriteFeature.ESCAPE_NON_ASCII.mappedFeature(), true)
+                            .writeValueAsString(baseResponse));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
