c5b7199cb1038c937e8140f84155475267eb300a
shin0112 <wnslcosltimo12@gmail.com>
2024-03-17 19:14:38 +0900

    feat: add memo entity
    Co-authored-by: soyeonvv <sovvworld@gmail.com>

---
+++ src/main/java/com/goormthon/tomado/domain/memo/entity/Memo.java
@@ -0,0 +1,39 @@
+package com.goormthon.tomado.domain.memo.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.goormthon.tomado.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public Memo(User user, String content) {
+        this.user = user;
+        this.content = content;
+    }
+
+}
