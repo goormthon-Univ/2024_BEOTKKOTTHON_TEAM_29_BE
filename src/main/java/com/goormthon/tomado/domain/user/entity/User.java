bea27c90f7e195a94e10087ef90354f7b509e484
shin0112 <wnslcosltimo12@gmail.com>
2024-03-18 22:38:44 +0900

    feat: user
    - 회원 가입
    - 아이디 중복 조회
    - 로그인
    - 회원 정보 조회
    - 회원 정부 수정
    - 회원 탈퇴

--- src/main/java/com/goormthon/tomado/domain/user/entity/User.java
+++ src/main/java/com/goormthon/tomado/domain/user/entity/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.goormthon.tomado.domain.category.entity.Category;
 import com.goormthon.tomado.domain.memo.entity.Memo;
 import com.goormthon.tomado.domain.task.entity.Task;
+import com.goormthon.tomado.domain.user.dto.UserChangeDto;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -55,6 +56,25 @@ public class User {
         this.loginId = loginId;
         this.password = password;
         this.nickname = nickname;
+    }
+
+    /**
+     * feat : 회원 정보 수정
+     *
+     * 정보가 수정될 때
+     * - 다른 값이 들어올 경우
+     *
+     * 정보가 수정되지 않을 때
+     * - 같은 값이 들어올 경우
+     * - null 값이 들어올 경우
+     */
+    public User change(UserChangeDto.Request request) {
+        this.loginId = loginId.equals(request.getLogin_id()) || request.getLogin_id() == null ? loginId : request.getLogin_id();
+        this.password = password.equals(request.getPassword()) || request.getPassword() == null ? password : request.getPassword();
+        this.nickname = nickname.equals(request.getNickname()) || request.getNickname() == null ? nickname : request.getNickname();
+        this.characterUrl = characterUrl.equals(request.getCharacter_url()) || request.getCharacter_url() == null ? characterUrl : request.getCharacter_url();
+
+        return this;
     }
 
 }
