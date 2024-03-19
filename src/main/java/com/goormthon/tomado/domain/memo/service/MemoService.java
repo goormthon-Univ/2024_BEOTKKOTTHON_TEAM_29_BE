ccfe4c2715cbafbdd8e1de52378a3e6a68640f97
shin0112 <wnslcosltimo12@gmail.com>
2024-03-19 23:27:35 +0900

    feat: setting for memo code

---
+++ src/main/java/com/goormthon/tomado/domain/memo/service/MemoService.java
@@ -0,0 +1,9 @@
+package com.goormthon.tomado.domain.memo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemoService {
+}
