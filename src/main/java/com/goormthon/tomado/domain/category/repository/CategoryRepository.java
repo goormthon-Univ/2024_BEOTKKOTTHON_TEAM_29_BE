03817b7cd546bd0f949cfb2350773698b8916f2d
soyeonvv <sovvworld@gmail.com>
2024-03-20 15:45:19 +0900

    refactor: 카테고리 검색 기준 수정

--- src/main/java/com/goormthon/tomado/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/goormthon/tomado/domain/category/repository/CategoryRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByUser(User user);
+    Optional<Category> findByIdAndUser_Id(Long categoryId, Long userId);
 }
