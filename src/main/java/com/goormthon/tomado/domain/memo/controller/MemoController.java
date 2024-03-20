05e6f6f2e63efeed6d6846568d3c6935846bc9f0
shin0112 <wnslcosltimo12@gmail.com>
2024-03-20 16:54:46 +0900

    refactor: controller request mapping url

--- src/main/java/com/goormthon/tomado/domain/memo/controller/MemoController.java
+++ src/main/java/com/goormthon/tomado/domain/memo/controller/MemoController.java
@@ -9,22 +9,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/memos")
 @RequiredArgsConstructor
 public class MemoController {
 
     public final MemoService memoService;
 
-    @GetMapping("/memos/{user_id}")
+    @GetMapping("/{user_id}")
     public ApiResponse<MemoDto.ResponseList> getMemoList(@PathVariable Long user_id) {
         return memoService.getMemoList(user_id);
     }
 
-    @PostMapping("/memos/{user_id}")
+    @PostMapping("/{user_id}")
     public ApiResponse write(@PathVariable Long user_id, @RequestBody MemoDto.Write write) {
         return memoService.write(user_id, write);
     }
 
-    @DeleteMapping("/memos")
+    @DeleteMapping("")
     public ApiResponse delete(@RequestParam(name = "user") Long user_id, @RequestParam("memo") Long memo_id) {
         return memoService.delete(user_id, memo_id);
     }
