90799b5cdf5e5c46cecbb2d593fdedd8608f6f58
soyeonvv <sovvworld@gmail.com>
2024-03-21 00:24:09 +0900

    refactor: DTO의 데이터 처리 코드 Service로 이동

--- src/main/java/com/goormthon/tomado/domain/category/dto/CategoryListDto.java
+++ src/main/java/com/goormthon/tomado/domain/category/dto/CategoryListDto.java
@@ -1,12 +1,10 @@
 package com.goormthon.tomado.domain.category.dto;
 
-import com.goormthon.tomado.domain.category.entity.Category;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,12 +16,7 @@ public class CategoryListDto {
         this.categoryList = categoryList;
     }
 
-    public static CategoryListDto from(List<Category> categoryList) {
-        List<Response> categoryDtoList = categoryList.stream()
-                .filter(category -> !category.isDeleted())
-                .map(category -> new Response(category.getId(), category.getTitle(), category.getColor(), category.getTomato()))
-                .collect(Collectors.toList());
-
-        return new CategoryListDto(categoryDtoList);
+    public static CategoryListDto from(List<Response> categoryList) {
+        return new CategoryListDto(categoryList);
     }
 }
