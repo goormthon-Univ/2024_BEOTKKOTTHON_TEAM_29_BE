68033018b7bfd00dd77b4dd371747f79b3920600
soyeonvv <sovvworld@gmail.com>
2024-03-20 18:30:16 +0900

    feat: task 생성

--- src/main/java/com/goormthon/tomado/domain/task/controller/TaskController.java
+++ src/main/java/com/goormthon/tomado/domain/task/controller/TaskController.java
@@ -1,12 +1,23 @@
 package com.goormthon.tomado.domain.task.controller;
 
+import com.goormthon.tomado.common.ApiResponse;
+import com.goormthon.tomado.domain.task.dto.TaskCreateDto;
 import com.goormthon.tomado.domain.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("tasks")
 public class TaskController {
 
     private final TaskService taskService;
+
+    @PostMapping
+    public ApiResponse<TaskCreateDto.Response> createTask(@RequestBody TaskCreateDto.Request request) {
+        return taskService.createTask(request);
+    }
 }
--- src/main/java/com/goormthon/tomado/domain/task/service/TaskService.java
+++ src/main/java/com/goormthon/tomado/domain/task/service/TaskService.java
@@ -1,9 +1,20 @@
 package com.goormthon.tomado.domain.task.service;
 
+import com.goormthon.tomado.common.ApiResponse;
+import com.goormthon.tomado.common.exception.NotFoundException;
+import com.goormthon.tomado.domain.category.entity.Category;
+import com.goormthon.tomado.domain.category.repository.CategoryRepository;
+import com.goormthon.tomado.domain.task.dto.TaskCreateDto;
+import com.goormthon.tomado.domain.task.entity.Task;
 import com.goormthon.tomado.domain.task.repository.TaskRepository;
+import com.goormthon.tomado.domain.user.entity.User;
+import com.goormthon.tomado.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.goormthon.tomado.common.response.ErrorMessage.*;
+import static com.goormthon.tomado.common.response.SuccessMessage.*;
 
 @Service
 @Transactional
@@ -11,4 +22,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ApiResponse<TaskCreateDto.Response> createTask(TaskCreateDto.Request request) {
+        User user = userRepository.findById(request.getUser_id())
+                .orElseThrow(() -> new NotFoundException(USER_NOT_EXIST));
+        Category category = categoryRepository.findById(request.getCategory_id())
+                .orElseThrow(() -> new NotFoundException(CATEGORY_NOT_EXIST));
+
+        Task task = new Task(user, request.getTitle(), category);
+        taskRepository.save(task);
+
+        return ApiResponse.success(TASK_SAVE_SUCCESS, TaskCreateDto.from(task));
+    }
 }
