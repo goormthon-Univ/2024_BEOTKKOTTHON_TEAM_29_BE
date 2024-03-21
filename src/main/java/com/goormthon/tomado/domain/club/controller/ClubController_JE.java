1aaf5e6b8e0f5d6a043c7124a54faf4dc0e3d8f5
shin0112 <wnslcosltimo12@gmail.com>
2024-03-22 03:05:15 +0900

    add: swagger operation

--- src/main/java/com/goormthon/tomado/domain/club/controller/ClubController_JE.java
+++ src/main/java/com/goormthon/tomado/domain/club/controller/ClubController_JE.java
@@ -5,6 +5,7 @@ import com.goormthon.tomado.domain.club.dto.ClubDto;
 import com.goormthon.tomado.domain.club.dto.ClubGetDto;
 import com.goormthon.tomado.domain.club.service.ClubService;
 import com.goormthon.tomado.domain.club.service.ClubService_JE;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ public class ClubController_JE {
 
     private final ClubService_JE clubService;
 
+    @Operation(summary = "클럽 정보 조회 - 단일")
     @GetMapping
     public ApiResponse<ClubGetDto.Response> getClub(@RequestParam(name = "user") Long userId, @RequestParam(name = "club") Long clubId) {
         return clubService.getClub(userId, clubId);
