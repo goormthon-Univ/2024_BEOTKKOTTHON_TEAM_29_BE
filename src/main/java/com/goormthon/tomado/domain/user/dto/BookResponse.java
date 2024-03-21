02d3c3726e03b34267db500b56ac31eedf552ed4
shin0112 <wnslcosltimo12@gmail.com>
2024-03-21 22:03:23 +0900

    chore: book reponse dto 세분화

--- src/main/java/com/goormthon/tomado/domain/user/dto/BookResponse.java
+++ src/main/java/com/goormthon/tomado/domain/user/dto/BookResponse.java
@@ -9,10 +9,6 @@ import java.time.LocalDateTime;
 
 public class BookResponse {
 
-    public static Simple from(UserTomado userTomado) {
-        return new Simple(userTomado.getTomado().getId(), userTomado.getTomado().getUrl(), userTomado.getTomado().getName(), userTomado.getCreatedAt());
-    }
-
     @Getter
     @NoArgsConstructor
     public static class Simple {
@@ -27,6 +23,35 @@ public class BookResponse {
             this.name = name;
             this.created_at = created_at;
         }
+
+        public static Simple from(UserTomado userTomado) {
+            return new Simple(userTomado.getTomado().getId(), userTomado.getTomado().getUrl(), userTomado.getTomado().getName(), userTomado.getCreatedAt());
+        }
     }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Detailed {
+        private Long tomado_id;
+        private String url;
+        private String name;
+        private String content;
+        private int tomato;
+        private LocalDateTime created_at;
+
+        public Detailed(Long tomado_id, String url, String name, String content, int tomato, LocalDateTime created_at) {
+            this.tomado_id = tomado_id;
+            this.url = url;
+            this.name = name;
+            this.content = content;
+            this.tomato = tomato;
+            this.created_at = created_at;
+        }
+
+        public static Detailed from(UserTomado userTomado) {
+            return new Detailed(userTomado.getTomado().getId(), userTomado.getTomado().getUrl(), userTomado.getTomado().getName(), userTomado.getTomado().getContent(), userTomado.getTomado().getTomato(), userTomado.getCreatedAt());
+        }
+    }
+
 
 }
